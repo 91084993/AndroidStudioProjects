ca6ba5735f9232b94d08e4538318dbf9cc06edf5
91084993 <31664271+91084993@users.noreply.github.com>
2018-04-03 09:59:26 -0700

    Added code to ChooseYourOwnAdventurev01

--- ChooseYourOwnAdventurev_01/app/src/main/java/com/example/palmdigital/chooseyourownadventurev_01/BackyardActivity.java
+++ ChooseYourOwnAdventurev_01/app/src/main/java/com/example/palmdigital/chooseyourownadventurev_01/BackyardActivity.java
@@ -1,13 +1,35 @@
 package com.example.palmdigital.chooseyourownadventurev_01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class BackyardActivity extends AppCompatActivity {
+public class BackyardActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_backyard);
+        Button buttonScreamObject = findViewById(R.id.buttonLeftScream);
+        buttonScreamObject.setOnClickListener(this);
+        Button buttonFaintObject = findViewById(R.id.buttonRightFaint);
+        buttonFaintObject.setOnClickListener(this);
     }
-}
+    public void onClick(View view)
+    {
+        if(view.getId() == R.id.buttonLeftScream)
+        {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+        else
+        {
+            Intent i = new Intent(this, EatenActivity.class);
+
+                startActivity(i);
+            }
+        }
+    }
+
---
+++ ChooseYourOwnAdventurev_01/app/src/main/java/com/example/palmdigital/chooseyourownadventurev_01/EatenActivity.java
@@ -0,0 +1,27 @@
+package com.example.palmdigital.chooseyourownadventurev_01;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class EatenActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eaten);
+        Button buttonRestartObject = findViewById(R.id.buttonRestart);
+        buttonRestartObject.setOnClickListener(this);
+    }
+
+    public void onClick(View view)
+    {
+        if(view.getId() == R.id.buttonRestart)
+        {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+}
