791c6dddad38cb64bfb2b5fe2e1a7fb5f9bfc70a
91084993 <31664271+91084993@users.noreply.github.com>
2018-04-04 08:24:28 -0700

    added more pages and more code

--- ChooseYourOwnAdventurev_01/app/src/main/java/com/example/palmdigital/chooseyourownadventurev_01/GoodEndActivity.java
+++ ChooseYourOwnAdventurev_01/app/src/main/java/com/example/palmdigital/chooseyourownadventurev_01/GoodEndActivity.java
@@ -6,26 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class WakeUpActivity extends AppCompatActivity implements View.OnClickListener
-{
+public class GoodEndActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wake_up);
+        setContentView(R.layout.activity_good_end);
         Button buttonRestartObject = findViewById(R.id.buttonRestart);
         buttonRestartObject.setOnClickListener(this);
-
-
     }
-
     public void onClick(View view)
     {
         if(view.getId() == R.id.buttonRestart)
         {
-            
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
         }
     }
-
 }
--- ChooseYourOwnAdventurev_01/app/src/main/java/com/example/palmdigital/chooseyourownadventurev_01/ToiletActivity.java
+++ ChooseYourOwnAdventurev_01/app/src/main/java/com/example/palmdigital/chooseyourownadventurev_01/ToiletActivity.java
@@ -1,13 +1,36 @@
 package com.example.palmdigital.chooseyourownadventurev_01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ToiletActivity extends AppCompatActivity {
+public class ToiletActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toilet);
+        Button buttonHECKYESObject = findViewById(R.id.ButtonRightHECKYES);
+        buttonHECKYESObject.setOnClickListener(this);
+        Button buttonYesObject = findViewById(R.id.ButtonLeftYes);
+        buttonYesObject.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view)
+    {
+        if(view.getId() == R.id.ButtonLeftYes)
+        {
+            Intent i = new Intent(this, GoodEndActivity.class);
+            startActivity(i);
+        }
+
+        if(view.getId() == R.id.ButtonRightHECKYES)
+        {
+            Intent i = new Intent(this, GoodEndActivity.class);
+            startActivity(i);
+        }
     }
 }
