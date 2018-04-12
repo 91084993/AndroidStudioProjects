1e5a46a5e16258189dcca3a9822d7c297b0d5468
91084993 <31664271+91084993@users.noreply.github.com>
2018-04-12 14:30:52 -0700

    finished

--- EditText01/app/src/main/java/com/example/palmdigital/edittext01/MainActivity.java
+++ EditText01/app/src/main/java/com/example/palmdigital/edittext01/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.palmdigital.edittext01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonGetNameObject = findViewById(R.id.buttonGetName);
+        buttonGetNameObject.setOnClickListener(this);
     }
+
+    public void onClick(View view)
+    {
+        EditText firstNameObject = (EditText) findViewById(R.id.et_firstName);
+        String firstStr = firstNameObject.getText().toString();
+        //Log.i("info", "First name is " + firstStr);
+
+        EditText LastNameObject = (EditText) findViewById(R.id.et_lName);
+        String LastStr = LastNameObject.getText().toString();
+
+        String fullName = firstStr+ " " + LastStr;
+
+        TextView tvOutput = findViewById(R.id.tvOutput);
+        tvOutput.setText(firstStr);
+
+
+    }
+
 }
